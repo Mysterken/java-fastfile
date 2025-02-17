5e03dbac634b0e1329f9117b53c7ef53ba99d606
Mysterken <Mysterkenzo@gmail.com>
2025-02-17 11:44:14 +0100

    initial commit

---
+++ src/main/java/com/group/javafastfile/JavaFastfileApplication.java
@@ -0,0 +1,13 @@
+package com.group.javafastfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFastfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFastfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/javafastfile/JavaFastfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.javafastfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFastfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
