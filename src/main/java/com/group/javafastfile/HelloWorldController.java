5680cd22271ce1a62304fa76ded0ac74ddda42f2
Mysterken <Mysterkenzo@gmail.com>
2025-02-17 17:02:40 +0100

    add hello world endpoint

---
+++ src/main/java/com/group/javafastfile/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.group.javafastfile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
