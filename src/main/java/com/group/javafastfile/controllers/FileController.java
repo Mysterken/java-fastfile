a90c5dd23a5892a5cc0459123720f60a5c9862ea
Mysterken <Mysterkenzo@gmail.com>
2025-02-18 09:35:23 +0100

    change project structure

--- src/main/java/com/group/javafastfile/controllers/FileController.java
+++ src/main/java/com/group/javafastfile/controllers/FileController.java
@@ -1,4 +1,4 @@
-package com.group.javafastfile;
+package com.group.javafastfile.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloWorldController {
+public class FileController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello, World!";
+    @GetMapping("/upload")
+    public String uploadFile() {
+        return "File uploaded!";
     }
 }
