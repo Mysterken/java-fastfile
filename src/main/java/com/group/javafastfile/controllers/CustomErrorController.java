1cd6f0c7e0e8758332e52ef410b4b847ce3b2243
Rayan <rayanatrouni@gmail.com>
2025-02-19 11:03:35 +0100

    Style error 404

--- src/main/java/com/group/javafastfile/controllers/CustomErrorController.java
+++ src/main/java/com/group/javafastfile/controllers/CustomErrorController.java
@@ -1,6 +1,11 @@
 package com.group.javafastfile.controllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +15,10 @@ public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
     @ResponseBody
-    public String handleError() {
-        return "Error 404 La page que vous recherchez n'existe pas.";
+    public ResponseEntity<Resource> getErrorPage() {
+        Resource resource = new ClassPathResource("static/error.html");
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE)
+                .body(resource);
     }
 }
